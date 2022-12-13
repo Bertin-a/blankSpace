cb7814cf0975f6911ba049a3a0a966fae7c9735e
Bertin-a <bertina.ayuure@amalitech.com>
2022-12-13 10:39:39 +0000

    Simple blankspace game

---
+++ blankspace.js
@@ -0,0 +1,7 @@
+function wordsBlank(myNoun, myAdjective, myVerb, myAdverb){
+    
+let result =" ";
+result+= "The "+myAdjective+" "+ myNoun+" "+myVerb+" to the store"
+return result
+}
+console.log(wordsBlank("cat", "small", "jumps", "quickly"));
